2dab3f26ae556269f6ed5c3b43b803edf4069a9a
Michael Ben Yosef <mishico5@gmail.com>
2021-01-31 19:50:20 +0200

    Add IUserRepository and AutoMapper to dependency injection

--- API/Exstensions/ApplicationServiceExtensions.cs
+++ API/Exstensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
 using API.Interfaces;
 using API.Services;
+using API.Util;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,7 +14,11 @@ namespace API.Exstensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            
+            services.AddScoped<IUserRepository, UserRepository>();
 
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
