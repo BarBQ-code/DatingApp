269bf0dbddda1d3ebbd09d4183d2a3c1e918d94d
Michael Ben Yosef <mishico5@gmail.com>
2021-02-01 19:20:26 +0200

    Add pogo class ApiException

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Errors
+{
+    public class ApiException
+    {
+        public int StuatusCode { get; set; }
+        public string Message { get; set; }
+        public string Detail { get; set; }
+
+        public ApiException(int stuatusCode, string message = null, string detail = null)
+        {
+            StuatusCode = stuatusCode;
+            Message = message;
+            Detail = detail;    
+        }
+    }
+}
