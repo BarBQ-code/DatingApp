868cc8192c36f366710207a1161cd1711c600c86
Michael Ben Yosef <mishico5@gmail.com>
2021-02-01 19:20:56 +0200

    Add extension method to calc the user's age

---
+++ API/Exstensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace API.Exstensions
+{
+    public static class DateTimeExstensions
+    {
+        public static int CalculateAge(this DateTime dateOfBirth)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - dateOfBirth.Year;
+
+            if (dateOfBirth.Date > today.AddYears(-age))
+                age--;
+            
+            return age;
+        }
+    }
+}
