7c1870b10a811c1476584b6338f374e122a7704a
Michael Ben Yosef <mishico5@gmail.com>
2021-02-02 21:08:01 +0200

    New .gitignore

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavComponent} from './nav/nav.component';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
+import {MemberListComponent} from './members/member-list/member-list.component';
+import {MemberDetailComponent} from './members/member-detail/member-detail.component';
+import {ListsComponent} from './lists/lists.component';
+import {MessagesComponent} from './messages/messages.component';
 import {SharedModule} from "./_models/shared.module";
-import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import {TestErrorsComponent} from './errors/test-errors/test-errors.component';
 import {ErrorInterceptor} from "./_interceptors/error.interceptor";
-import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { ServerErrorComponent } from './errors/server-error/server-error.component';
-import { MemberCardComponent } from './members/member-card/member-card.component';
+import {NotFoundComponent} from './errors/not-found/not-found.component';
+import {ServerErrorComponent} from './errors/server-error/server-error.component';
+import {MemberCardComponent} from './members/member-card/member-card.component';
 import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
-
+import {MemberEditComponent} from './members/member-edit/member-edit.component';
+import {LoadingInterceptor} from "./_interceptors/loading.interceptor";
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
     TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    MemberCardComponent
+    MemberCardComponent,
+    MemberEditComponent,
   ],
   imports: [
     BrowserModule,
@@ -43,11 +45,12 @@ import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
