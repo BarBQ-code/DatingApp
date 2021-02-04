6f9ed28d94e0879328978e8ab3b526c489852c08
Michael Ben Yosef <mishico5@gmail.com>
2021-02-04 19:26:28 +0200

    Export Reactive forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -22,6 +22,9 @@ import {MemberCardComponent} from './members/member-card/member-card.component';
 import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
 import {MemberEditComponent} from './members/member-edit/member-edit.component';
 import {LoadingInterceptor} from "./_interceptors/loading.interceptor";
+import {PhotoEditorComponent} from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
+import { DateInputComponent } from './_forms/date-input/date-input.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +41,9 @@ import {LoadingInterceptor} from "./_interceptors/loading.interceptor";
     ServerErrorComponent,
     MemberCardComponent,
     MemberEditComponent,
+    PhotoEditorComponent,
+    TextInputComponent,
+    DateInputComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,6 +51,7 @@ import {LoadingInterceptor} from "./_interceptors/loading.interceptor";
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
   ],
   providers: [
