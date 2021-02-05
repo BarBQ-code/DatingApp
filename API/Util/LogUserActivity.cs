95d914cff937d1b8811b2be16df0a9b38420c83d
Michael Ben Yosef <mishico5@gmail.com>
2021-02-05 22:52:28 +0200

    Add LogUserActivity service to update last active property

---
+++ API/Util/LogUserActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using API.Exstensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Util
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+                return;
+
+            var userId = resultContext.HttpContext.User.GetId();
+            var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
+
+            var user = await repo.GetUserByIdAsync(userId);
+            user.LastActive = DateTime.Now;
+
+            await repo.SaveAllAsync();
+        }
+    }
+}
