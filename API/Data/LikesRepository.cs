278b09609dbbb93b7df2e45ca949f08c69dbfc7f
Michael Ben Yosef <mishico5@gmail.com>
2021-02-06 18:29:42 +0200

    Add A LikesRepository to handle likes related calls to the database

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Exstensions;
+using API.Interfaces;
+using API.Util;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class LikesRepository : ILikesReposirtory
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LikesRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+        {
+            return await _context.Likes.FindAsync(sourceUserId, likedUserId);
+        }
+
+        public async Task<AppUser> GetUserWithLikes(int userId)
+        {
+            return await _context.Users
+                .Include(x => x.LikedUsers)
+                .FirstOrDefaultAsync(x => x.Id == userId);
+        }
+
+        public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
+        {
+            var users = _context.Users
+                .OrderBy(u => u.UserName)
+                .AsQueryable();
+            var likes = _context.Likes.AsQueryable();
+
+            if (likesParams.Predicate == "liked")
+            {
+                likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
+                users = likes.Select(like => like.LikedUser);
+            }
+            else if (likesParams.Predicate == "likedBy")
+            {
+                likes = likes.Where(like => like.LikedUserId == likesParams.UserId);
+                users = likes.Select(like => like.SourceUser);
+            }
+            else
+            {
+                throw new UnsupportedContentTypeException($"Predicate provided is unsupported, predicate name: {nameof(likesParams.Predicate)}");
+            }
+
+            var likedUsers = users.Select(user => new LikeDto
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                KnownAs = user.KnownAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).Url,
+                City = user.City,
+            });
+
+            return await PagedList<LikeDto>.CreateAsync(likedUsers, likesParams.PageNumber, likesParams.PageSize);
+        }
+    }
+}
