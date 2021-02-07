a2d3d86765764c8c7c6ff677b888d4e8a4066759
Michael Ben Yosef <mishico5@gmail.com>
2021-02-07 22:03:22 +0200

    Add MessageEntity migration

--- API/Data/Migrations/20210207142412_MessageEntityAdded.Designer.cs
+++ API/Data/Migrations/20210207142412_MessageEntityAdded.Designer.cs
@@ -4,14 +4,16 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210207142412_MessageEntityAdded")]
+    partial class MessageEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
