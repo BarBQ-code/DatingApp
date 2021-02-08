150c53c531479f9acd96c24dd7e66a7d58425a8a
Michael Ben Yosef <mishico5@gmail.com>
2021-02-08 21:26:11 +0200

    Add AppRole to inherit from IdentityRole and help with many to many relationships

--- API/Entities/AppRole.cs
+++ API/Entities/AppRole.cs
@@ -1,7 +1,10 @@
-﻿namespace API.Entities
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities
 {
-    public class AppRole
+    public class AppRole: IdentityRole<int>
     {
-        
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
