cff4de529b7226437500778035929bb19812fb2d
Michael Ben Yosef <mishico5@gmail.com>
2021-02-09 17:04:22 +0200

    Add connection entity to help with mainting web socket connections

---
+++ API/Entities/Connection.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Entities
+{
+    public class Connection
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+
+        public Connection() { } //ctor for enitity framework
+
+        public Connection(string connectionId, string userName)
+        {
+            ConnectionId = connectionId;
+            UserName = userName;
+        }
+    }
+}
