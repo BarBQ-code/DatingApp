5ccc9ff68739be449c71a12ff96d7e7ff4a55fd0
Michael Ben Yosef <mishico5@gmail.com>
2021-02-09 17:04:51 +0200

    Add group entity to help with web socket grouping

---
+++ API/Entities/Group.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities
+{
+    public class Group
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public ICollection<Connection> Connections { get; set; } = new List<Connection>();
+
+        public Group() { } //ctor for entity framework
+
+        public Group(string name)
+        {
+            Name = name;
+        }
+    }
+}
