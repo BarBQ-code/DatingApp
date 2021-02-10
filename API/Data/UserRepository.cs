731a019d82e8c9e36c86bee1985836de36b80463
Michael Ben Yosef <mishico5@gmail.com>
2021-02-10 11:20:12 +0200

    Optimize queries

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -23,17 +23,12 @@ namespace API.Data
             _context = context;
             _mapper = mapper;
         }
-        
+
         public void Update(AppUser user)
         {
             _context.Entry(user).State = EntityState.Modified;
         }
-
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
-
+        
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
             return await _context.Users
@@ -81,6 +76,14 @@ namespace API.Data
                 .Where(user => user.UserName == username)
                 .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
                 .SingleOrDefaultAsync();
+        }
+
+        public async Task<string> GetUserGender(string username)
+        {
+            return await _context.Users
+                .Where(user => user.UserName == username)
+                .Select(user => user.Gender)
+                .FirstOrDefaultAsync();
         }
     }
 }
