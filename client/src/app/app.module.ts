99072251dcfe54e01ae6c6ffe4f2d1761dc36f9c
Michael Ben Yosef <mishico5@gmail.com>
2021-02-10 11:27:58 +0200

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,13 +23,14 @@ import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
 import {MemberEditComponent} from './members/member-edit/member-edit.component';
 import {LoadingInterceptor} from "./_interceptors/loading.interceptor";
 import {PhotoEditorComponent} from './members/photo-editor/photo-editor.component';
-import { TextInputComponent } from './_forms/text-input/text-input.component';
-import { DateInputComponent } from './_forms/date-input/date-input.component';
-import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
-import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
-import { HasRoleDirective } from './_directives/has-role.directive';
-import { UserManagementComponent } from './admin/user-management/user-management.component';
-import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
+import {TextInputComponent} from './_forms/text-input/text-input.component';
+import {DateInputComponent} from './_forms/date-input/date-input.component';
+import {MemberMessagesComponent} from './members/member-messages/member-messages.component';
+import {AdminPanelComponent} from './admin/admin-panel/admin-panel.component';
+import {HasRoleDirective} from './_directives/has-role.directive';
+import {UserManagementComponent} from './admin/user-management/user-management.component';
+import {RolesModalComponent} from './modals/roles-modal/roles-modal.component';
+import {ConfirmDialogComponent} from './modals/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
@@ -54,6 +55,7 @@ import { RolesModalComponent } from './modals/roles-modal/roles-modal.component'
     HasRoleDirective,
     UserManagementComponent,
     RolesModalComponent,
+    ConfirmDialogComponent,
   ],
   imports: [
     BrowserModule,
