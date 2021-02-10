2e040fbfe702a342ecc30dace9da0ae0fe5c779d
Michael Ben Yosef <mishico5@gmail.com>
2021-02-10 11:46:03 +0200

    Remove import for app error component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,7 +14,6 @@ import {MemberDetailComponent} from './members/member-detail/member-detail.compo
 import {ListsComponent} from './lists/lists.component';
 import {MessagesComponent} from './messages/messages.component';
 import {SharedModule} from "./_models/shared.module";
-import {TestErrorsComponent} from './errors/test-errors/test-errors.component';
 import {ErrorInterceptor} from "./_interceptors/error.interceptor";
 import {NotFoundComponent} from './errors/not-found/not-found.component';
 import {ServerErrorComponent} from './errors/server-error/server-error.component';
@@ -23,14 +22,14 @@ import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
 import {MemberEditComponent} from './members/member-edit/member-edit.component';
 import {LoadingInterceptor} from "./_interceptors/loading.interceptor";
 import {PhotoEditorComponent} from './members/photo-editor/photo-editor.component';
-import {TextInputComponent} from './_forms/text-input/text-input.component';
-import {DateInputComponent} from './_forms/date-input/date-input.component';
-import {MemberMessagesComponent} from './members/member-messages/member-messages.component';
-import {AdminPanelComponent} from './admin/admin-panel/admin-panel.component';
-import {HasRoleDirective} from './_directives/has-role.directive';
-import {UserManagementComponent} from './admin/user-management/user-management.component';
-import {RolesModalComponent} from './modals/roles-modal/roles-modal.component';
-import {ConfirmDialogComponent} from './modals/confirm-dialog/confirm-dialog.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
+import { DateInputComponent } from './_forms/date-input/date-input.component';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
+import { ConfirmDialogComponent } from './modals/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +41,6 @@ import {ConfirmDialogComponent} from './modals/confirm-dialog/confirm-dialog.com
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
-    TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
