c1d8fc7617032bf0d34ac414df2d20ef35bcd962
Michael Ben Yosef <mishico5@gmail.com>
2021-02-10 11:23:44 +0200

    Remove SaveAllAsync method

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -10,9 +10,6 @@ namespace API.Interfaces
     public interface IUserRepository
     {
         void Update(AppUser user);
-        
-        Task<bool> SaveAllAsync();
-
         Task<IEnumerable<AppUser>> GetUsersAsync();
 
         Task<AppUser> GetUserByIdAsync(int id);
@@ -22,5 +19,6 @@ namespace API.Interfaces
         Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
 
         Task<MemberDto> GetMemberAsync(string username);
+        Task<string> GetUserGender(string username);
     }
 }
