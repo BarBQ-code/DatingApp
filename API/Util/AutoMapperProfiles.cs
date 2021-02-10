e3b4cf14e7712de86468d65004010f5afaa41582
Michael Ben Yosef <mishico5@gmail.com>
2021-02-10 11:24:43 +0200

    Remove mapping from normal date time to Utc date time, another souliton was found

--- API/Util/AutoMapperProfiles.cs
+++ API/Util/AutoMapperProfiles.cs
@@ -30,9 +30,6 @@ namespace API.Util
                 .ForMember(dest => dest.RecipientPhotoUrl,
                     opt => opt.MapFrom(
                         src => src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
-            
-            CreateMap<DateTime, DateTime>()
-                .ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
         }
     }
 }
