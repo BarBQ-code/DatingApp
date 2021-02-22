ff7afd2ab8d6eabeb2aefe94e22ec3f73b7dc87f
Michael Ben Yosef <mishico5@gmail.com>
2021-02-22 12:20:42 +0200

    Add Utc Extension for ApplicationDbContext

--- API/Exstensions/DateTimeExtensions.cs
+++ API/Exstensions/DateTimeExtensions.cs
@@ -1,9 +1,58 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Exstensions
 {
     public static class DateTimeExstensions
     {
+        /// <summary>
+        /// Work around for a known Entity framework problem with Utc, the framework doesn't save in the db any indication that the date format is of type Utc
+        /// Code taken from this issue on the Entity framework core repo
+        /// See: <see cref="https://github.com/dotnet/efcore/issues/4711"/>
+        /// </summary>
+        private const String IsUtcAnnotation = "IsUtc";
+        private static readonly ValueConverter<DateTime, DateTime> UtcConverter =
+            new ValueConverter<DateTime, DateTime>(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+        private static readonly ValueConverter<DateTime?, DateTime?> UtcNullableConverter =
+            new ValueConverter<DateTime?, DateTime?>(v => v, v => v == null ? v : DateTime.SpecifyKind(v.Value, DateTimeKind.Utc));
+
+        public static PropertyBuilder<TProperty> IsUtc<TProperty>(this PropertyBuilder<TProperty> builder, Boolean isUtc = true) =>
+            builder.HasAnnotation(IsUtcAnnotation, isUtc);
+
+        public static Boolean IsUtc(this IMutableProperty property) =>
+            ((Boolean?)property.FindAnnotation(IsUtcAnnotation)?.Value) ?? true;
+
+        /// <summary>
+        /// Make sure this is called after configuring all your entities.
+        /// </summary>
+        public static void ApplyUtcDateTimeConverter(this ModelBuilder builder)
+        {
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (!property.IsUtc())
+                    {
+                        continue;
+                    }
+
+                    if (property.ClrType == typeof(DateTime))
+                    {
+                        property.SetValueConverter(UtcConverter);
+                    }
+
+                    if (property.ClrType == typeof(DateTime?))
+                    {
+                        property.SetValueConverter(UtcNullableConverter);
+                    }
+                }
+            }
+        }
+
         public static int CalculateAge(this DateTime dateOfBirth)
         {
             DateTime today = DateTime.Today;
