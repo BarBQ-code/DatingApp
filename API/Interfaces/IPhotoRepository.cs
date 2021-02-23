5bc6a96535dabcc0fa2af9aa1e4c9043bdc1db57
Michael Ben Yosef <mishico5@gmail.com>
2021-02-23 14:58:29 +0200

    Add IPhotoRepository

---
+++ API/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IPhotoRepository
+    {
+        Task<Photo> GetPhotoById(int id);
+        void RemovePhoto(Photo photo);
+        Task<IEnumerable<PhotoForApprovalDto>> GetUnapprovedPhotos();
+    }
+}
