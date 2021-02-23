5fce4ea41ffc33ae5f2fb86dd536dec19cf9cdf8
Michael Ben Yosef <mishico5@gmail.com>
2021-02-23 15:04:15 +0200

    Update to new GetMemberAsync func

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -18,7 +18,8 @@ namespace API.Interfaces
 
         Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
 
-        Task<MemberDto> GetMemberAsync(string username);
+        Task<MemberDto> GetMemberAsync(string username, bool isCurrentUser);
         Task<string> GetUserGender(string username);
+        public Task<AppUser> GetUserByPhotoId(int photoId);
     }
 }
