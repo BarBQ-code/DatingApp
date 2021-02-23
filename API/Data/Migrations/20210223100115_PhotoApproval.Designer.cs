8490eb7dae16b50a28702c79f056d77f5b4d4d3a
Michael Ben Yosef <mishico5@gmail.com>
2021-02-23 14:58:02 +0200

    Add new PhotoApproval migration

--- API/Data/Migrations/20210223100115_PhotoApproval.Designer.cs
+++ API/Data/Migrations/20210223100115_PhotoApproval.Designer.cs
@@ -4,14 +4,16 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210223100115_PhotoApproval")]
+    partial class PhotoApproval
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
